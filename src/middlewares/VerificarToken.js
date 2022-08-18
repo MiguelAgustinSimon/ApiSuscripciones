47eabe3d216c05d767007280421cdb42a8ab7442
nicolas laugas <nicolas.laugas.ext@errepar.com>
2022-08-18 05:12:53 +0000

    fix middleware

--- src/middlewares/VerificarToken.js
+++ src/middlewares/VerificarToken.js
@@ -1,30 +1,27 @@
 var jwt = require('jsonwebtoken');
-const { connectLogger } = require('log4js');
 //Authorization: Barer <token>
 const VerificarToken =(req, res, next) => {
-    
     try {
         const bearerHeader = req.headers['authorization'];
-        let bearerToken = '';
-        if(typeof bearerHeader !== 'undefined') {
-          bearerToken = bearerHeader;
-          console.log(process.env.PUBLIC_KEY)
-          jwt.verify(bearerToken, process.env.PUBLIC_KEY, (error, authData) => {
-            //req.token.expiresIn= '365d';
-    
-            if(error){
-              console.log(error.message);
-              res.sendStatus(403);
-            }else{
-              next();
-            }
-          })
+        if(typeof bearerHeader !== 'undefined'){
+          const bearerToken = bearerHeader.split(" ")[1];
+          req.token = bearerToken;
+        }else{
+          res.sendStatus(403);
         }
-        else res.sendStatus(403);
       } catch (error) {
         return res.status(403).json({message: "Ocurrio un error al autenticar el token. Verifique su token por favor.", error: error})
       }
-      
+     
+      jwt.verify(req.token, process.env.PUBLIC_KEY, (error, authData) => {
+        //req.token.expiresIn= '365d';
+
+        if(error){
+          res.sendStatus(403);
+        }else{
+          next();
+        }
+      })
   }
   
   module.exports = VerificarToken;
